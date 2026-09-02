049bd81b8f0e51df92a92185424d83ab618da0b5
chandu437u <chandupadarthi8116@gmail.com>
2026-09-02 06:13:49 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,1 +1,12 @@
-MY NAME IS PADARTHI THIMOTHI PURSUING MY B.TECH 4 TH IN THE STREAM OF CYBER SECURITY
+public class FibonacciExample {
+    public static void main(String[] args) {
+        int n1 = 0, n2 = 1, count = 5;
+        System.out.print("First " + count + " numbers: ");
+        for (int i = 1; i <= count; ++i) {
+            System.out.print(n1 + " ");
+            int sum = n1 + n2;
+            n1 = n2;
+            n2 = sum;
+        }
+    }
+}
